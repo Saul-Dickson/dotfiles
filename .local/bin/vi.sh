56dd15ca9f7e3a8df5fc1a8980d4fa4b708ae356
Saul-Dickson <acepebisboy@icloud.com>
2020-10-30 17:12:19 -0500

    modified:   vi.sh

--- .local/bin/vi.sh
+++ .local/bin/vi.sh
@@ -1,5 +1,7 @@
 [ -z $1 ] &&
-    [ -d $1 ] && vifm $@ || \
-    [ -L $1 ] && vifm $@ || nvim $@ || \
-    [ -z $1 ] && vifm $(pwd) || \
+    [ -d $1 ] && vifm $@ ; exit || 
+    [ -L $1 ] && vifm $@ ; exit || 
+        nvim $@ ; exit || 
+    \
+    [ -z $1 ] && vifm . ; exit || 
     nvim $@
