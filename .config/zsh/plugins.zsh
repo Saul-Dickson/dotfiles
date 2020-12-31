b8bf3545f184f2cc85cc01355e27ec25b07f23e5
Saul-Dickson <acepebisboy@icloud.com>
2020-12-31 12:28:09 -0600

    new file:   plugins.zsh 	new file:   prompt.zsh 	new file:   settings.zsh 	new file:   vicursor.zsh

---
+++ .config/zsh/plugins.zsh
@@ -0,0 +1,34 @@
+# set the plugin directory
+PLUGINDIR=~/.config/zsh/plugins
+
+### Zsh Autosuggestions ###
+source $PLUGINDIR/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+
+### Zsh Completions ###
+source $PLUGINDIR/zsh-completions/zsh-completions.plugin.zsh
+
+
+### Fast Syntax Highlighting ###
+source $PLUGINDIR/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+
+### Zsh Autocomplete ###
+source $PLUGINDIR/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+
+### Zsh Abbreviations ###
+source $PLUGINDIR/zsh-abbr/zsh-abbr.zsh
+
+### Warp Directory ###
+source $PLUGINDIR/wd/wd.plugin.zsh
+
+# ### z.lua chdir plugin ###
+# source $PLUGINDIR/z.lua/z.lua.plugin.zsh
+
+### gitstatus is sourced in prompt.zsh ###
+
+### Powerlevel10k Prompt ###
+# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+# fi
+
+# To customize prompt, run `p10k configure` or edit $PLUGINDIR/powerlevel10k/.p10k.zsh.
+# [[ ! -f $PLUGINDIR/powerlevel10k/.p10k.zsh ]] || source $PLUGINDIR/powerlevel10k/.p10k.zsh
---
+++ .config/zsh/settings.zsh
@@ -0,0 +1,20 @@
+# Set histfile
+HISTFILE=~/.cache/zsh/histfile
+HISTSIZE=100
+
+# Basic auto/tab complete:
+autoload -U compinit
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+compinit && _comp_options+=(globdots)		# Include hidden files.
+
+# Use vim keys in tab complete menu:
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
+
+# vi mode
+bindkey -v
+export KEYTIMEOUT=1
