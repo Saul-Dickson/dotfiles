922a882b7c668d1d86c7843946163f78d286cf32
Saul-Dickson <acepebisboy@icloud.com>
2021-03-25 12:38:32 -0500

    modified:   .config/zsh/.chpwd-recent-dirs 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 ## set $gitPath ##
 gitPath="$HOME/.local/src/github.com/Saul-Dickson/dotfiles"
 
-## install homebrew ##
+###############################
+# Install Programs & Dotfiles #
+###############################
+
+## Install Homebrew ##
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
 
-## Add brew to PATH ##
-[[ $OSTYPE != *"darwin"* ]] &&
-    export PATH="/home/linuxbrew/.linuxbrew/bin"
+################
+## Alias Brew ##
+################
+if [[ $OSTYPE != *"darwin"* ]]; then
+    alias brew="/home/linuxbrew/.linuxbrew/bin/brew"
+else
+    alias brew="/usr/local/bin/brew"
+fi
 
 ## Install the various packages ##
 brew install bat cmake ctags exa fd fzf gh git go imagemagick lua make mpv \
@@ -15,6 +24,23 @@ brew install bat cmake ctags exa fd fzf gh git go imagemagick lua make mpv \
 
 mkdir $HOME/.local/src/github.com/Saul-Dickson -p
 git clone https://github.com/Saul-Dickson/dotfiles $gitPath
+
+## Install other, less important packages using pip ##
+sudo pip3.9 install epr ueberzug wifi-password
+
+## Install other, less important packages using go ##
+go get github.com/zquestz/s
+
+## Install unpackaged applications ##
+mkdir -p ".local/apps"
+
+mkdir -p ".local/apps/royalroad-downloader"
+wget \
+    "https://github.com/Aivean/royalroad-downloader/releases/download/2.2.0/royalroad-downloader-assembly-2.2.0.jar" \
+    ~/.local/apps/royalroad-downloader
+
+## Install other, less important packages using npm ##
+npm install --global trash-cli
 
 ####################
 # Symlink Dotfiles #
@@ -37,22 +63,9 @@ ln -sF $(echo $gitPath)/.local/wall ~/.local/wall
 ## Create symlink to zshenv ##
 ln -sF $(echo $gitPath)/.zshenv ~/.zshenv
 
-## Install other, less important packages using pip ##
-sudo pip3.9 install epr ueberzug wifi-password
-
-## Install other, less important packages using go ##
-go get github.com/zquestz/s
-
-## Install unpackaged applications ##
-mkdir -p ".local/apps"
-
-mkdir -p ".local/apps/royalroad-downloader"
-wget \
-    "https://github.com/Aivean/royalroad-downloader/releases/download/2.2.0/royalroad-downloader-assembly-2.2.0.jar" \
-    ~/.local/apps/royalroad-downloader
-
-## Install other, less important packages using npm ##
-npm install --global trash-cli
-
 ## Change the default shell to zsh ##
 sudo chsh /usr/local/bin/zsh
+
+## Create directories for configuration, data storage and caching ##
+mkdir $HOME/.cache/zsh # Zsh Caching
+mkdir $HOME/.cache/nvim # NeoVIM UndoTrees
